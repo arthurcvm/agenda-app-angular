11a3ac25f657eda50eeffd1c0ba725d157c2f895
Arthur Cezar <arthurcvm@hotmail.com>
2024-08-29 21:43:21 -0300

    paginação de contatos

--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -5,6 +5,7 @@ import { ContatoService } from '../contato.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { MatDialog } from '@angular/material/dialog'
 import { ContatoDetalheComponent } from '../contato-detalhe/contato-detalhe.component';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-contato',
@@ -17,6 +18,11 @@ export class ContatoComponent implements OnInit {
   contatos: Contato[] = []; 
   colunas = ['foto', 'id', 'nome', 'email', 'favorito'];
 
+  totalElementos = 0;
+  pagina = 0;
+  tamanho = 10;
+  pageSizeOptions : number[] = [10];
+
   constructor(
     private service : ContatoService,
     private fb : FormBuilder,
@@ -25,7 +31,7 @@ export class ContatoComponent implements OnInit {
 
   ngOnInit(): void {
     this.montarFormulario();
-    this.listarContatos();
+    this.listarContatos(this.pagina, this.tamanho);
     
   }
 
@@ -36,9 +42,11 @@ export class ContatoComponent implements OnInit {
     })
   }
 
-  listarContatos(){
-    this.service.list().subscribe(response => {
-      this.contatos = response;
+  listarContatos(pagina = 0, tamanho = 10){
+    this.service.list(pagina, tamanho).subscribe(response => {
+      this.contatos = response.content;
+      this.totalElementos = response.totalElements;
+      this.pagina = response.number;
     })
   }
 
@@ -74,6 +82,11 @@ export class ContatoComponent implements OnInit {
       height: '450px',
       data: contato
     })
+  }
+
+  paginar(event: PageEvent){
+    this.pagina = event.pageIndex;
+    this.listarContatos(this.pagina, this.tamanho);
   }
 
 }
